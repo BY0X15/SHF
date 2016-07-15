226d1abda2bd70bf596d5e2391b5b0064f680877
Dzmitry Straltsou <Dzmitry_Straltsou@epam.com>
2016-07-15 18:06:32 +0300

    Added home page witch is display weather else other information

---
+++ app/components/pages/home.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'homePage',
+  templateUrl: 'app/template/pages/home.html',
+  directives: []
+})
+
+export class homePage {
+
+}
