e4a15f152957ddb512b062e223df1abc6706b70f
Dzmitry Straltsou <Dzmitry_Straltsou@epam.com>
2016-07-18 18:30:03 +0300

    Small changes in the room selection panel

--- app/components/pages/temperature.js
+++ app/components/pages/temperature.js
@@ -22,7 +22,10 @@ let temperaturePage = class temperaturePage {
         this.blockCurTemp = {
             transform: "rotate(-68deg)"
         };
+        this.isMovePanel = false;
         this.isAutoTemperature = false;
+        this.onPanelTopPX = '41px';
+        this.firstClickY = 0;
     }
     rotate(curTemp) {
         const min = -68;
@@ -48,6 +51,38 @@ let temperaturePage = class temperaturePage {
         if (curTemperature >= this.minTemperature) {
             this.rotate(curTemperature);
             this.valueTemperature = curTemperature.toString();
+        }
+    }
+    onMouseEvent(event) {
+        if (event.type === 'touchstart') {
+            this.isMovePanel = true;
+        }
+        if (event.type === 'touchend') {
+            this.isMovePanel = false;
+            if (event.srcElement.className !== 'header')
+                return;
+            let px = event.changedTouches[0].screenY - 530;
+            if (px > 42) {
+                px = 42;
+            }
+            else {
+                px = -300;
+            }
+            this.onPanelTopPX = `${px}px`;
+        }
+        if (event.type === 'touchmove' && this.isMovePanel === true) {
+            if (event.srcElement.className !== 'header')
+                return;
+            let minY = 174;
+            let maxY = 530;
+            let px = event.touches[0].screenY - 530;
+            if (px > 41) {
+                px = 42;
+            }
+            else if (px < -299) {
+                px = -300;
+            }
+            this.onPanelTopPX = `${px}px`;
         }
     }
 };
